d7e7d1f760dc6083f3027c1be8ce847fc1a57523
setouchi-h <setouchi.kazuki@gmail.com>
2023-08-07 15:51:28 +0900

    initial commit

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title A Raffle Contract
+ * @author k-h
+ * @dev Implements Chainlink VRFv2
+ */
+contract  Raffle {
+    constructor() {
+        
+    }
+}
